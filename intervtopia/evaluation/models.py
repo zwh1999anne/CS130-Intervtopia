ec2e94b50b818e1509f268fa2a60fc437bf5b68e
Haofan Lu <haofan21@ucla.edu>
2022-10-30 22:59:30 -0700

    evaluation app draft

---
+++ intervtopia/evaluation/models.py
@@ -0,0 +1,26 @@
+from email.policy import default
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    '''
+    target: who will answer this question
+        value = 0: both interviewer and interviewee should answer
+        value = 1: the question is for interviewer only
+        value = -1: the question is for interviewee only
+    '''
+    target = models.IntegerField(default=0) 
+
+    def __str__(self) -> str:
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    selected = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.choice_text
---
+++ intervtopia/evaluation/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'evaluation'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('interviewer/', views.interviewer, name = 'interviewer'),
+    path('interviewee/', views.interviewee, name = 'interviewee'),
+    path('submit/', views.submit, name='submit')
+]
