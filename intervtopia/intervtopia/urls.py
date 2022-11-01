7b6709a93182b589dcd89b1fadac6cb4066544f2
Haofan Lu <haofan21@ucla.edu>
2022-10-31 19:47:30 -0700

    user account init

--- intervtopia/intervtopia/urls.py
+++ intervtopia/intervtopia/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('evaluation/', include('evaluation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
