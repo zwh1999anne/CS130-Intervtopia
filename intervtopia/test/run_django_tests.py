a98b0d1c441cd770f21cbc11d71bc9567fa1e35a
Yadi Cao <yadicao95@ucla.edu>
2022-11-01 22:01:38 -0700

    unit test wrapper

--- intervtopia/test/run_django_tests.py
+++ intervtopia/test/run_django_tests.py
@@ -15,6 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    sys.argv.append('test')
+    sys.argv.append('test/django_tests')
     execute_from_command_line(sys.argv)
 
 
