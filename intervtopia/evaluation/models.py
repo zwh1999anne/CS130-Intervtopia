2f2fd0cfe9dd28055f86548c5a5d1e7169781146
kelihuang <nickcoopercs1998@gmail.com>
2022-11-04 09:25:31 -0700

    add test case in evaluation and interview

--- intervtopia/evaluation/models.py
+++ intervtopia/evaluation/models.py
@@ -12,23 +12,22 @@ class Question(models.Model):
     question_ranking: use to rank question, the smaller number, the higher position
     question_name: use in the update method
     """
-    target_choices = [('ER', "Interviewer"), ('EE', "Interviewee")]
+    question_name = models.CharField(max_length=200)
     question_text = models.CharField(max_length=200)
+    target_choices = [('ER', "Interviewer"), ('EE', "Interviewee")]
     target = models.CharField(max_length=2, default='ER', choices=target_choices)
     question_ranking = models.IntegerField(default=0)
-    question_name = models.CharField(max_length=200)
 
     def __str__(self) -> str:
         return self.question_text
 
-
 class Choice(models.Model):
     """
-        question: ask question
-        choice_value: value for nthis choice
-        choice_text: the text of the chioce
-        selected: boolean for if selected
-        """
+    question: ask question
+    choice_value: value for nthis choice
+    choice_text: the text of the chioce
+    selected: boolean for if selected
+    """
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_value = models.IntegerField(default=0)
@@ -37,61 +36,6 @@ class Choice(models.Model):
 
     def __str__(self) -> str:
         return self.choice_text
-
-
-# class InterviewER_EvaluationFormDB(models.Model):
-#     interviewee = models.CharField(max_length=200, default="")
-#     score = models.IntegerField(default=0)
-#     problem_solving = models.CharField(max_length=200, default="")
-#     communication = models.CharField(max_length=200, default="")
-#     coding_skill = models.CharField(max_length=200, default="")
-
-#     def __str__(self) -> str:
-#         return self.interviewee
-
-#     def insert(self):
-#         raise NotImplementedError
-
-#     def remove(self):
-#         raise NotImplementedError
-
-#     def update(self, request):
-#         interviewee_text = request.POST['interviewee']
-#         score_int = request.POST['choice']
-#         problem_solving_sentence = request.POST['problem-solving']
-#         communication_sentence = request.POST['communication']
-#         coding_skill_sentence = request.POST['coding-skill']
-
-#         self.objects.create(interviewee=interviewee_text, score=score_int, problem_solving=problem_solving_sentence, communication=communication_sentence, coding_skill=coding_skill_sentence)
-
-#     def query(self):
-#         raise NotImplementedError
-
-# class InterviewEE_EvaluationFormDB(models.Model):
-#     interviewer = models.CharField(max_length=200, default="")
-#     score = models.IntegerField(default=0)
-#     helpful = models.CharField(max_length=200, default="")
-#     communication = models.CharField(max_length=200, default="")
-
-#     def __str__(self) -> str:
-#         return self.interviewer
-
-#     def insert(self):
-#         raise NotImplementedError
-
-#     def remove(self):
-#         raise NotImplementedError
-
-#     def update(self, request):
-#         interviewer_text = request.POST['interviewer']
-#         score_int = request.POST['choice']
-#         helpful_sentence = request.POST['helpful']
-#         communication_sentence = request.POST['communication']
-
-#         self.objects.create(interviewer=interviewer_text, score=score_int, helpful=helpful_sentence, communication=communication_sentence)
-
-#     def query(self):
-#         raise NotImplementedError
 
 
 class Response(models.Model):
@@ -103,12 +47,6 @@ class Response(models.Model):
 
     def __str__(self) -> str:
         return self.name
-
-    def update(self, request):
-        '''
-        TODO: update the response
-        '''
-
 
 class EvalForm(models.Model):
     name = models.CharField(max_length=100, default="")
@@ -122,29 +60,3 @@ class EvalForm(models.Model):
 
     def __str__(self) -> str:
         return self.name
-
-    def onSubmit(self):
-        '''
-        TODO: update the response and write to DB
-        '''
-
-    def getReport(self):
-        '''
-        TODO: generate report from the response
-        '''
-
-    def getQuestions(self):
-        '''
-        TODO: return the questions
-        '''
-
-    @staticmethod
-    def update(request):
-        '''
-        TODO: update the response to the evaluation form
-        '''
-
-        # if 'interviewee' in request.POST:
-        #     InterviewER_EvaluationFormDB.update(InterviewER_EvaluationFormDB, request)
-        # else:
-        #     InterviewEE_EvaluationFormDB.update(InterviewEE_EvaluationFormDB, request)
--- intervtopia/test/django_tests/test_interview.py
+++ intervtopia/test/django_tests/test_interview.py
@@ -1,18 +1,181 @@
 from django.test import TestCase, Client
-from interview.models import Problem, ProblemDBUpdater
+from interview.models import *
 from external.leetCodeWrapper import leetCodeQuestionQuery, interviewQuestion
 from django.urls import reverse
 # Create your tests here.
 
 
-def import_questions_leetcode(number_qs: int):
-    pb = ProblemDBUpdater()
-    pb.addProblems(number_qs)
+class InterviewerModelTests(TestCase):
+
+    def test_crud_a_interviewer(self):
+
+        test_dict_response = {'name': "Test response", 'problem_solving': 2,
+                              'communication': 3, 'coding_skill': 4, 'helpful': 1}
+
+        response = Response.objects.create(**test_dict_response)
+        user1 = CustomUser.objects.create(username='carlo')
+        user2 = CustomUser.objects.create(username='david')
+
+        test_dict_eval = {'name': "Test response", 'rating': 2, 'comments': 'no comment', 'response': response,
+                          'targer_user': user1, 'target_role': 'EE'}
+
+        evalform = EvalForm.objects.create(**test_dict_eval)
+
+        test_dict = {'user': user1, 'evalForm': evalform}
+
+        # create
+        interviewer = Interviewer.objects.create(**test_dict)
+
+        self.assertEqual(interviewer.user.username, "carlo")
+        self.assertEqual(interviewer.evalForm.name, "Test response")
+
+        # read
+        interviewer_set = Interviewer.objects.filter(**test_dict)
+
+        for interviewer in interviewer_set.all():
+            self.assertEqual(interviewer.user.username, "carlo")
+            self.assertEqual(interviewer.evalForm.name, "Test response")
+
+        # update
+        update_dict = {'user': user2}
+
+        test_dict.update(update_dict)
+
+        interviewer_set.update(**update_dict)
+        interviewer_set = Interviewer.objects.filter(**test_dict)
+
+        for interviewer in interviewer_set.all():
+            self.assertEqual(interviewer.user.username, "david")
+            self.assertEqual(interviewer.evalForm.name, "Test response")
+
+        # delete
+
+        interviewer_set.delete()
+
+        interviewer_set = Interviewer.objects.filter(**test_dict)
+
+        self.assertEqual(interviewer_set.count(), 0)
+
+        user1.delete()
+        user2.delete()
+        response.delete()
+
+        evalform.delete()
 
 
-class QuestionModelTests(TestCase):
+class InterviewModelTests(TestCase):
 
-    def test_question_import_leetcode(self):
-        import_questions_leetcode(40)
-        q_count = Problem.objects.all().count()
-        self.assertEqual(q_count, 40)
+    def test_crud_a_interview(self):
+
+        test_dict_response = {'name': "Test response", 'problem_solving': 2,
+                              'communication': 3, 'coding_skill': 4, 'helpful': 1}
+
+        response = Response.objects.create(**test_dict_response)
+
+        user1 = CustomUser.objects.create(username='carlo')
+        user2 = CustomUser.objects.create(username='david')
+
+        test_dict_eval_ee = {'name': "Test response", 'rating': 2, 'comments': 'no comment', 'response': response,
+                             'targer_user': user1, 'target_role': 'EE'}
+
+        test_dict_eval_er = {'name': "Test response", 'rating': 2, 'comments': 'no comment', 'response': response,
+                             'targer_user': user2, 'target_role': 'ER'}
+
+        evalform_ee = EvalForm.objects.create(**test_dict_eval_ee)
+        evalform_er = EvalForm.objects.create(**test_dict_eval_er)
+
+        interviewer = Interviewer.objects.create(user=user1, evalForm=evalform_er)
+        interviewee = Interviewee.objects.create(user=user2, evalForm=evalform_ee)
+
+        test_dict = {'name': "Test interview", 'viewER': interviewer, 'viewEE': interviewee,
+                     'date_and_time': '2022-11-04 14:30', 'room_link': 'www.google.com'}
+
+        # create
+        interview = Interview.objects.create(**test_dict)
+
+        self.assertEqual(interview.viewER.user.username, "carlo")
+        self.assertEqual(interview.viewEE.user.username, "david")
+        self.assertEqual(interview.name, "Test interview")
+
+        # read
+        interview_set = Interview.objects.filter(**test_dict)
+
+        for interview in interview_set.all():
+            self.assertEqual(interview.viewER.user.username, "carlo")
+            self.assertEqual(interview.viewEE.user.username, "david")
+            self.assertEqual(interview.name, "Test interview")
+
+        # update
+        update_dict = {'date_and_time': '2022-11-04 14:50'}
+
+        test_dict.update(update_dict)
+
+        interview_set.update(**update_dict)
+        interview_set = Interview.objects.filter(**test_dict)
+
+        for interview in interview_set.all():
+            self.assertEqual(interview.viewER.user.username, "carlo")
+            self.assertEqual(interview.viewEE.user.username, "david")
+            self.assertEqual(interview.name, "Test interview")
+            self.assertEqual(interview.date_and_time, '2022-11-04 14:50')
+
+        # delete
+
+        interview_set.delete()
+
+        interview_set = Interview.objects.filter(**test_dict)
+
+        self.assertEqual(interview_set.count(), 0)
+
+        interviewer.delete()
+        interviewee.delete()
+
+        user1.delete()
+        user2.delete()
+        response.delete()
+
+
+class ProblemModelTests(TestCase):
+
+    def test_crud_a_problem(self):
+
+        test_dict = {'problem_name': "Test problem", 'problem_url': 'www.google.com',
+                     'problem_statement': 'this is a test problem', 'problem_difficulty': 'M'}
+
+        # create
+        problem = Problem.objects.create(**test_dict)
+
+        self.assertEqual(problem.problem_name, "Test problem")
+        self.assertEqual(problem.problem_url, "www.google.com")
+        self.assertEqual(problem.problem_statement, "this is a test problem")
+        self.assertEqual(problem.problem_difficulty, "M")
+
+        # read
+        problem_set = Problem.objects.filter(**test_dict)
+
+        for problem in problem_set.all():
+            self.assertEqual(problem.problem_name, "Test problem")
+            self.assertEqual(problem.problem_url, "www.google.com")
+            self.assertEqual(problem.problem_statement, "this is a test problem")
+            self.assertEqual(problem.problem_difficulty, "M")
+
+        # update
+        update_dict = {'problem_url': 'www.baidu.com', 'problem_difficulty': 'H'}
+
+        test_dict.update(update_dict)
+
+        problem_set.update(**update_dict)
+        problem_set = Problem.objects.filter(**test_dict)
+
+        for problem in problem_set.all():
+            self.assertEqual(problem.problem_name, "Test problem")
+            self.assertEqual(problem.problem_url, "www.baidu.com")
+            self.assertEqual(problem.problem_statement, "this is a test problem")
+            self.assertEqual(problem.problem_difficulty, "H")
+
+        # delete
+        problem_set.delete()
+
+        problem_set = Problem.objects.filter(**test_dict)
+
+        self.assertEqual(problem_set.count(), 0)
