a3735693d2ac89556b36b1089a6b1b71e9d959e9
zwh1999anne <zwh1999anne@gmail.com>
2022-11-19 12:35:58 -0800

    add match button responsiveness

---
+++ intervtopia/frontend/src/backend_data/match_list.js
@@ -0,0 +1,16 @@
+const match_info = {
+    name: "Wendy Y",
+    first_language: "JavaScipt",
+    second_language: "Python",
+    desired_difficulty: "hard",
+    available_day: "Tuesday",
+    available_time: "10:00 - 11:00 A.M.",
+    evaluation_score: "4.7"
+}
+
+export function matchConfirmed(matchName, matchDay, matchTime){
+    return 0;
+}
+export function getMatchInfo(matchType){
+   return match_info;
+}
