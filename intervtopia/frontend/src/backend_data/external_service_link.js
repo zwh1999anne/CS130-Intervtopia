96fb04c5923e1eb748710684b382803edd53fa2c
Yadi Cao <elliott.yd.cao@outlook.com>
2022-11-20 16:19:54 -0800

    done join meeting popup

---
+++ intervtopia/frontend/src/backend_data/external_service_link.js
@@ -0,0 +1,10 @@
+// TODO: streaming from backend
+
+const service_link = 
+    {
+      "question": "https://leetcode.com/problems/valid-number",
+      "chatting": "https://meet.jit.si/3361307c5b48ef4f",
+      "IDE": "https://codeshare.io/3361307c5b48ef4f"
+    };
+
+  export default service_link;
